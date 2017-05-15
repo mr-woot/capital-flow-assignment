eafc86b68670f1bc4202b80c7fbb2fe5f81fab05
mr-woot <tusharmudgal3@gmail.com>
2017-05-15 18:31:16 +0530

    https changes

--- public/js/services/MainService.js
+++ public/js/services/MainService.js
@@ -5,7 +5,7 @@ angular
 function MainService($http) {
   return {
     getTopAlbums: function () {
-        var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=843f438cd2f8d32dd7eefa9f06edfd7f&format=json";
+        var url = "https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=843f438cd2f8d32dd7eefa9f06edfd7f&format=json";
         return $http.get(url);
     },
     postTrackHistory: function (data) {

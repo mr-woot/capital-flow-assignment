f72cabb2b375863ed7c34d88fd24bcbec4017b46
mr-woot <tusharmudgal3@gmail.com>
2017-05-15 18:20:55 +0530

    procfile added

--- public/js/services/MainService.js
+++ public/js/services/MainService.js
@@ -7,6 +7,11 @@ function MainService($http) {
     getTopAlbums: function () {
         var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=843f438cd2f8d32dd7eefa9f06edfd7f&format=json";
         return $http.get(url);
+    },
+    postTrackHistory: function (data) {
+        // var url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=rj&api_key=843f438cd2f8d32dd7eefa9f06edfd7f&format=json";
+        var url = "";
+        return $http.post(url, data);
     }
   };
 }

c4ddfa3afdd0ea2b5ea3a7d2ed94ffcfbe669c83
mr-woot <tusharmudgal3@gmail.com>
2017-05-23 19:52:32 +0530

    feat: 2-way bind tags

--- public/js/controllers/SearchCtrl.js
+++ public/js/controllers/SearchCtrl.js
@@ -1,3 +1,4 @@
+let test;
 (function () {
   angular
     .module('SearchCtrl', [])
@@ -5,7 +6,7 @@
 
   function SearchController(SearchService, ngProgressFactory) {
     var vm = this;
-
+    test = vm;
     vm.loader = ngProgressFactory.createInstance();
 
     vm.searchResults = [];
@@ -13,7 +14,7 @@
     vm.searchTrack = searchTrack;
     vm.searchKey = "";
     vm.searchFlag = false;
-    vm.hFlag = false;
+    vm.hFlag = -1;
 
     vm.searchHistoryList = [];
 
@@ -21,17 +22,14 @@
       vm.loader.start();
       SearchService.searchTrack(track)
         .then(function (response) {
-          console.log(response);
           vm.searchResults = response.data.results.trackmatches.track;
         })
         .then(function(){
           postSearchHistory(track);
         })
         .finally(function () {
-            vm.searchFlag = true;
+            vm.searchFlag = 1;
             vm.loader.complete();
-            getSearchHistory();
-            vm.hFlag = false;
         });
     }
 
@@ -39,7 +37,10 @@
       if (!track) return;
       SearchService.postSearchHistory(track)
       .then(function(response) {
-        console.log(JSON.stringify(response, undefined, 2));
+        if (vm.searchHistoryList.indexOf(track) === -1) {
+            vm.searchHistoryList.push(track);
+        }
+        vm.hFlag = 1;
       })
       .catch(function(err) {
         console.log(JSON.stringify(error));
@@ -51,21 +52,19 @@
       .then(function(response) {
         var array = response.data.history;
         vm.searchHistoryList = array.map(item => item.query).filter((value, index, self) => self.indexOf(value) === index)
+        if (response.data.count === 0) {
+          vm.hFlag = 0;
+          vm.searchHistoryList.splice(0, vm.searchHistoryList.length);
+        } else {
+          vm.hFlag = 1;
+        }
       })
       .catch(function(error) {
         console.log(JSON.stringify(error));
-      })
-      .finally(function() {
-        if (!vm.searchHistoryList) {
-          vm.hFlag = false;
-        } else {
-          vm.hFlag = true;
-        }
       });
     }
 
     function init () {
-        vm.searchFlag = false;
         getSearchHistory();
     }
     init();
